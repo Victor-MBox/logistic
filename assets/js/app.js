793965be48d5040b6f2424e5de4597640fdb4172
Victor-MBox <victor.matsakov7@gmail.com>
2023-02-13 23:08:34 +0400

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,75 @@
-$(function() {
+$(function () {
+  let intro = $("#intro");
+  let header = $("#header");
+  let introH = intro.innerHeight();
+  let headerH = header.innerHeight();
 
-    let intro = $("#intro");
-    let header = $("#header");
-    let introH = intro.innerHeight();
-    let headerH = header.innerHeight();
-
-
-    /* Header class on scroll
+  /* Header class on scroll
     =====================================*/
 
+  headerScroll();
+
+  $(window).on("scroll  resize", function () {
     headerScroll();
+  });
 
-    $(window).on("scroll  resize", function() {
-        headerScroll();
-    });
+  function headerScroll() {
+    introH = intro.innerHeight();
+    headerH = header.innerHeight();
 
-    function headerScroll() {
-        introH = intro.innerHeight();
-        headerH = header.innerHeight();
+    let scrollTop = $(this).scrollTop();
 
-        let scrollTop = $(this).scrollTop();
-
-        if( scrollTop >= (introH - headerH) ) {
-            header.addClass("header--dark");
-        } else {
-            header.removeClass("header--dark");
-        }
+    if (scrollTop >= introH - headerH) {
+      header.addClass("header--dark");
+    } else {
+      header.removeClass("header--dark");
     }
+  }
 
-
-    /* Smooth Scroll to sections
+  /* Smooth Scroll to sections
     =====================================*/
 
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
 
+    let scrollEl = $(this).data("scroll");
+    let scrollElPos = $(scrollEl).offset().top;
 
+    $("html, body").animate(
+      {
+        scrollTop: scrollElPos - headerH,
+      },
+      500
+    );
+  });
+
+  /* ScrollSpy
+    =====================================*/
+  let windowH = $(window).height();
+  scrollSpy(scrollTop);
+
+  $(window).on("scroll", function () {
+    scrollTop = $(this).scrollTop();
+    scrollSpy(scrollTop);
+  });
+
+  function scrollSpy(scrollTop) {
+    $("[data-scrollspy]").each(function () {
+      let $this = $(this);
+      let sectionId = $this.data("scrollspy");
+      let sectionOffset = $this.offset().top;
+      sectionOffset = sectionOffset - windowH * 0.33333;
+
+      if (scrollTop >= sectionOffset) {
+        $("#nav [data-scroll]").removeClass("active");
+        $('#nav [data-scroll="' + sectionId + '"]').addClass("active");
+      }
+
+      if (scrollTop == 0) {
+        $("#nav [data-scroll]").removeClass("active");
+      }
     });
+  }
 
-
-
-
+  
 });
