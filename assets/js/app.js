65722d91ded7772dc73ea6ca3738574fcb69f747
Victor-MBox <victor.matsakov7@gmail.com>
2023-02-13 21:02:35 +0400

    1

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    let intro = $("#intro");
+    let header = $("#header");
+    let introH = intro.innerHeight();
+    let headerH = header.innerHeight();
+
+
+    /* Header class on scroll
+    =====================================*/
+
+    headerScroll();
+
+    $(window).on("scroll  resize", function() {
+        headerScroll();
+    });
+
+    function headerScroll() {
+        introH = intro.innerHeight();
+        headerH = header.innerHeight();
+
+        let scrollTop = $(this).scrollTop();
+
+        if( scrollTop >= (introH - headerH) ) {
+            header.addClass("header--dark");
+        } else {
+            header.removeClass("header--dark");
+        }
+    }
+
+
+    /* Smooth Scroll to sections
+    =====================================*/
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+
+    });
+
+
+
+
+});
